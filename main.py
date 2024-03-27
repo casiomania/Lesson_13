1bbfea2030c9fd2424befdacc665d51856ca7919
Serhii <casiomania@proton.me>
2024-03-27 21:24:55 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# Lesson 13 / Homework
+# Create a class hierarchy to describe the academy.
+# Sample list of classes: Person, Teacher, Student, Subject, Academy, etc.
+# Think about the architecture: implement the principles of OOP
